6f6407db3e388b2a27b4bf1653473436a72b7269
Chavez Pettit <84909678+chavez6@users.noreply.github.com>
2021-06-04 18:11:16 -0700

    1.9

--- pageCode.js
+++ pageCode.js
@@ -1,13 +1,14 @@
-function print(){
-    console.log('pressed')
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.querySelector('#login');
+    const createAccountForm = document.querySelector('#createAccount');
 
-function submitForm() {
-    // Get the first form with the name, 
-    // Hopefully there is only one, 
-    // but there are more, select the correct index
-    var frm = document.getElementsByName('formbro')[0];
-    frm.submit(); // Submit
-    frm.reset();  // Reset
-    return false; // Prevent page refresh
- }
+    document.querySelector('#linkCreateAccount').addEventListener('click', () => {
+        loginForm.classList.add('form--hidden');
+        createAccountForm.classList.remove('form--hidden');
+    });
+
+    document.querySelector('#linkLogin').addEventListener('click', () => {
+        loginForm.classList.remove('form--hidden');
+        createAccountForm.classList.add('form--hidden');
+    });
+});
